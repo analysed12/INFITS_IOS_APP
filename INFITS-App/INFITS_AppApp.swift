80afd54bff9b023a48bda85ffa8ae72f8fbe2348
rajnibanduni <rajnibanduni5@gmail.com>
2023-03-10 12:27:26 +0530

    Initial Commit

---
+++ INFITS-App/INFITS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  INFITS_AppApp.swift
+//  INFITS-App
+//
+//  Created by Rajni Banduni on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct INFITS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
